266f54472b05a20e8e520837319171c6ac0c4708
David House <davidahouse@gmail.com>
2014-06-24 08:19:47 -0400

    created the view class

---
+++ Example/LoadingIndicatorExample/LoadingIndicatorExample/DHLoadingIndicatorView.h
@@ -0,0 +1,13 @@
+//
+//  DHLoadingIndicatorView.h
+//  LoadingIndicatorExample
+//
+//  Created by David House on 6/23/14.
+//  Copyright (c) 2014 Random Accident. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DHLoadingIndicatorView : UIView
+
+@end
